2a14caf9450531fd1f5935644816c18e86e17ab4
Almujahid Jamion <94305038+firemelon96@users.noreply.github.com>
2026-01-29 21:39:24 +0800

    new dist

--- dist/config/swagger.js
+++ dist/config/swagger.js
@@ -4,7 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.swaggerSpec = void 0;
+const path_1 = __importDefault(require("path"));
 const swagger_jsdoc_1 = __importDefault(require("swagger-jsdoc"));
+const isProd = process.env.NODE_ENV === 'production';
+// __dirname here is inside dist/config in production, and src/config in dev.
+const apisGlob = isProd
+    ? path_1.default.join(__dirname, '../routes/**/*.js') // dist/routes/**/*.js
+    : path_1.default.join(__dirname, '../routes/**/*.ts'); // src/routes/**/*.ts (if running ts-node)
 exports.swaggerSpec = (0, swagger_jsdoc_1.default)({
     definition: {
         openapi: '3.0.0',
@@ -38,5 +44,5 @@ exports.swaggerSpec = (0, swagger_jsdoc_1.default)({
             },
         ],
     },
-    apis: ['./src/routes/**/*.ts'], // scan route files
+    apis: [apisGlob], // scan route files
 });
